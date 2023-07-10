f92a9acbbd9571cd6cd44bb5ee96ad242d03936f
Ricardo Delgado <rickydel19@gmail.com>
2023-07-10 13:46:56 -0500

    Updated meal show page for ingredients and ingredient/quantity

--- src/MealsShowPage.jsx
+++ src/MealsShowPage.jsx
@@ -24,31 +24,46 @@ export function MealsShowPage(props) {
     props.onDestroyMeal(meal);
   };
 
-  return (
-    <div id="meals-show">
-      <h2>{meal.name}</h2>
-      <img src={meal.picture} alt="" />
-      {/* <p>Ingredients: {meal.ingredients}</p> */}
+  if (meal.meal_ingredients) {
+    return (
+      <div id="meals-show">
+        <h2>{meal.name}</h2>
+        <img src={meal.picture} alt="" />
+        {console.log(meal)}
+        <p>Ingredients: </p>
+        <ul>
+          {meal.meal_ingredients.map((meal_ingredient) => (
+            <div key={meal_ingredient.id}>
+              <li>
+                {meal_ingredient.ingredient.name} - Quantity: {meal_ingredient.ingredient_quantity}
+              </li>
+            </div>
+          ))}
+        </ul>
 
-      <h1>Edit Meal</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          Title: <input name="name" defaultValue={meal.name} type="text" />
-        </div>
-        <div>
-          Picture: <input name="picture" defaultValue={meal.picture} type="text" />
-        </div>
-        {/* <div>
-          Ingredients: <input name="ingredients" defaultValue={meal.ingredients} type="text" />
-        </div> */}
-        <button className="btn btn-warning" type="submit">
-          Update Meal
+        <h1>Edit Meal</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            Name: <input name="name" defaultValue={meal.name} type="text" />
+          </div>
+          <div>
+            Picture: <input name="picture" defaultValue={meal.picture} type="text" />
+          </div>
+          <button className="btn btn-warning" type="submit">
+            Update Meal
+          </button>
+        </form>
+
+        <button className="btn btn-danger" onClick={handleClick}>
+          Destroy Meal
         </button>
-      </form>
-
-      <button className="btn btn-danger" onClick={handleClick}>
-        Destroy Meal
-      </button>
-    </div>
-  );
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 }
