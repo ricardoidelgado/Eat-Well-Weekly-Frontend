3945f22862412930429a8d71926fe22c8b02570d
Ricardo Delgado <rickydel19@gmail.com>
2023-07-13 15:52:42 -0500

    Added search for daily and weekly meal plans

--- src/DailyMealPlansIndex.jsx
+++ src/DailyMealPlansIndex.jsx
@@ -1,32 +1,40 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export function DailyMealPlansIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
   return (
     <div id="dmp-index">
       <h1>All Daily Meal Plans</h1>
       <Link className="btn btn-success" to="/daily_meal_plans/new">
         Create New Daily Meal Plan
       </Link>
-      {props.dailyMealPlans?.map((dailyMealPlan) => (
-        <div key={dailyMealPlan.id}>
-          <h2>{dailyMealPlan.name}</h2>
-          <ul>
-            <li>Breakfast: {dailyMealPlan.breakfast_meal?.name}</li>
-            <img src={dailyMealPlan.breakfast_meal?.picture} alt="" />
-          </ul>
-          <ul>
-            <li>Lunch: {dailyMealPlan.lunch_meal?.name}</li>
-            <img src={dailyMealPlan.lunch_meal?.picture} alt="" />
-          </ul>
-          <ul>
-            <li>Dinner: {dailyMealPlan.dinner_meal?.name}</li>
-            <img src={dailyMealPlan.dinner_meal?.picture} alt="" />
-          </ul>
-          <Link className="btn btn-primary" to={`/daily_meal_plans/${dailyMealPlan.id}`}>
-            More Details
-          </Link>
-        </div>
-      ))}
+      <div>
+        Search
+        <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} />
+      </div>
+      {props.dailyMealPlans
+        ?.filter((dailyMealPlan) => dailyMealPlan.name.toLowerCase().includes(searchFilter.toLowerCase()))
+        .map((dailyMealPlan) => (
+          <div key={dailyMealPlan.id}>
+            <h2>{dailyMealPlan.name}</h2>
+            <ul>
+              <li>Breakfast: {dailyMealPlan.breakfast_meal?.name}</li>
+              <img src={dailyMealPlan.breakfast_meal?.picture} alt="" />
+            </ul>
+            <ul>
+              <li>Lunch: {dailyMealPlan.lunch_meal?.name}</li>
+              <img src={dailyMealPlan.lunch_meal?.picture} alt="" />
+            </ul>
+            <ul>
+              <li>Dinner: {dailyMealPlan.dinner_meal?.name}</li>
+              <img src={dailyMealPlan.dinner_meal?.picture} alt="" />
+            </ul>
+            <Link className="btn btn-primary" to={`/daily_meal_plans/${dailyMealPlan.id}`}>
+              More Details
+            </Link>
+          </div>
+        ))}
     </div>
   );
 }
--- src/WeeklyMealPlansIndex.jsx
+++ src/WeeklyMealPlansIndex.jsx
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export function WeeklyMealPlansIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
   return (
     <div>
       <h1>All Weekly Meal Plans</h1>
       <Link className="btn btn-success" to="/weekly_meal_plans/new">
         Create New Weekly Meal Plan
       </Link>
-      {props.weeklyMealPlans.map((weeklyMealPlan) => (
-        <div key={weeklyMealPlan.id}>
-          <h2>{weeklyMealPlan.name}</h2>
-          <Link className="btn btn-primary" to={`/weekly_meal_plans/${weeklyMealPlan.id}`}>
-            More Details
-          </Link>
-        </div>
-      ))}
+      <div>
+        Search
+        <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} />
+      </div>
+      {props.weeklyMealPlans
+        .filter((weeklyMealPlan) => weeklyMealPlan.name.toLowerCase().includes(searchFilter.toLowerCase()))
+        .map((weeklyMealPlan) => (
+          <div key={weeklyMealPlan.id}>
+            <h2>{weeklyMealPlan.name}</h2>
+            <Link className="btn btn-primary" to={`/weekly_meal_plans/${weeklyMealPlan.id}`}>
+              More Details
+            </Link>
+          </div>
+        ))}
     </div>
   );
 }
