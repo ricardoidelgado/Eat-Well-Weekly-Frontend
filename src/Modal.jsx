b97d9de388b7d964993399a8e83acfc569b8c1b9
Ricardo Delgado <rickydel19@gmail.com>
2023-07-19 19:20:07 -0500

    Added ability to close modal by clicking outside

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -3,8 +3,8 @@ import "./Modal.css";
 export function Modal(props) {
   if (props.show) {
     return (
-      <div className="modal-background">
-        <section className="modal-main">
+      <div className="modal-background" onClick={props.onClose}>
+        <section className="modal-main" onClick={(event) => event.stopPropagation()}>
           {props.children}
           <button className="close" type="button" onClick={props.onClose}>
             &#x2715;
