37d315db0fc55b6c80d129db60f3ea12c19e39b4
Ricardo <rickydel19@gmail.com>
2023-07-23 23:49:46 -0500

    Added styling to MealIngredient show edit action

--- src/MealIngredientsShow.jsx
+++ src/MealIngredientsShow.jsx
@@ -2,7 +2,9 @@ export function MealIngredientsShow(props) {
   const handleSubmit = (event) => {
     event.preventDefault();
     const params = new FormData(event.target);
-    props.onUpdateMealIngredient(props.mealIngredient.id, params, () => event.target.reset());
+    props.onUpdateMealIngredient(props.mealIngredient.id, params, () =>
+      event.target.reset()
+    );
     window.location.href = `/meals/${props.meal.id}`; // Change this to hide a modal, redirect to a specific page, etc.
   };
 
@@ -14,13 +16,27 @@ export function MealIngredientsShow(props) {
     <div>
       <h1>Ingredients for {props.meal.name}</h1>
       <form onSubmit={handleSubmit}>
-        <li>
-          {props.mealIngredient.ingredient.name} - Quantity:
-          <input name="ingredient_quantity" defaultValue={props.mealIngredient.ingredient_quantity} type="number" />
-        </li>
-        <button type="submit">Update Ingredient</button>
+        <div className="row">
+          <div className="mb-2 col-3">
+            <label htmlFor="mealIngredient" className="form-label">
+              {props.mealIngredient?.ingredient.name} - Quantity
+            </label>
+            <input
+              name="ingredient_quantity"
+              type="number"
+              className="form-control"
+              id="mealIngredient"
+              defaultValue={props.mealIngredient?.ingredient_quantity}
+            />
+          </div>
+        </div>
+        <button className="btn btn-warning col-6" type="submit">
+          Update Ingredient
+        </button>
+        <button className="btn btn-danger col-6" onClick={handleClick}>
+          Remove Ingredient
+        </button>
       </form>
-      <button onClick={handleClick}>Remove Ingredient</button>
     </div>
   );
 }
