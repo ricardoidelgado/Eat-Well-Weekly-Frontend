4c2542a937b6886426f607d81522a58a407f0705
Ricardo <rickydel19@gmail.com>
2023-07-23 23:36:48 -0500

    Added styling to MealsShow edit action

--- src/MealsShowPage.jsx
+++ src/MealsShowPage.jsx
@@ -6,13 +6,16 @@ import { MealIngredientsNew } from "./MealIngredientsNew";
 export function MealsShowPage(props) {
   const [meal, setMeal] = useState({});
   const params = useParams();
-  const [newMealIngredientVisibility, setNewMealIngredientVisibility] = useState(false);
+  const [newMealIngredientVisibility, setNewMealIngredientVisibility] =
+    useState(false);
   const [editMealVisibility, setEditMealVisibility] = useState(false);
 
   const handleShowMeal = () => {
-    axios.get(`http://localhost:3000/meals/${params.id}.json`).then((response) => {
-      setMeal(response.data);
-    });
+    axios
+      .get(`http://localhost:3000/meals/${params.id}.json`)
+      .then((response) => {
+        setMeal(response.data);
+      });
   };
 
   useEffect(handleShowMeal, []);
@@ -34,18 +37,34 @@ export function MealsShowPage(props) {
       <h3 className="mt-3">Ingredients: </h3>
 
       {meal.meal_ingredients?.map((mealIngredient) => (
-        <ul className="list-group list-group-horizontal row" key={mealIngredient.id}>
-          <li className="list-group-item col-2">{mealIngredient.ingredient.name} </li>
-          <li className="list-group-item col-2">Quantity: {mealIngredient.ingredient_quantity.toLocaleString("en-US", {maximumFractionDigits: 0})} </li>
+        <ul
+          className="list-group list-group-horizontal row"
+          key={mealIngredient.id}
+        >
           <li className="list-group-item col-2">
-            <button className="btn btn-warning" onClick={() => props.onShowMealIngredient(mealIngredient, meal)}>
+            {mealIngredient.ingredient.name}{" "}
+          </li>
+          <li className="list-group-item col-2">
+            Quantity:{" "}
+            {mealIngredient.ingredient_quantity.toLocaleString("en-US", {
+              maximumFractionDigits: 0,
+            })}{" "}
+          </li>
+          <li className="list-group-item col-2">
+            <button
+              className="btn btn-warning"
+              onClick={() => props.onShowMealIngredient(mealIngredient, meal)}
+            >
               Update Ingredient
             </button>
           </li>
         </ul>
       ))}
 
-      <button className="btn btn-success mt-3" onClick={() => setNewMealIngredientVisibility(true)}>
+      <button
+        className="btn btn-success mt-3"
+        onClick={() => setNewMealIngredientVisibility(true)}
+      >
         Add New Ingredient
       </button>
       {newMealIngredientVisibility ? (
@@ -69,55 +88,120 @@ export function MealsShowPage(props) {
         <tbody className="table-group-divider">
           <tr>
             <td>Calories</td>
-            <td>{meal.nutritional_summary?.calories.toLocaleString("en-US", {maximumFractionDigits: 0})}</td>
+            <td>
+              {meal.nutritional_summary?.calories.toLocaleString("en-US", {
+                maximumFractionDigits: 0,
+              })}
+            </td>
           </tr>
           <tr>
             <td>Fat</td>
-            <td>{meal.nutritional_summary?.fat.toLocaleString("en-US", {maximumFractionDigits: 0})}</td>
+            <td>
+              {meal.nutritional_summary?.fat.toLocaleString("en-US", {
+                maximumFractionDigits: 0,
+              })}
+            </td>
           </tr>
           <tr>
             <td>Sodium</td>
-            <td>{meal.nutritional_summary?.sodium.toLocaleString("en-US", {maximumFractionDigits: 0})}</td>
+            <td>
+              {meal.nutritional_summary?.sodium.toLocaleString("en-US", {
+                maximumFractionDigits: 0,
+              })}
+            </td>
           </tr>
           <tr>
             <td>Carbs</td>
-            <td>{meal.nutritional_summary?.carbs.toLocaleString("en-US", {maximumFractionDigits: 0})}</td>
+            <td>
+              {meal.nutritional_summary?.carbs.toLocaleString("en-US", {
+                maximumFractionDigits: 0,
+              })}
+            </td>
           </tr>
           <tr>
             <td>Protein</td>
-            <td>{meal.nutritional_summary?.protein.toLocaleString("en-US", {maximumFractionDigits: 0})}</td>
+            <td>
+              {meal.nutritional_summary?.protein.toLocaleString("en-US", {
+                maximumFractionDigits: 0,
+              })}
+            </td>
           </tr>
           <tr>
             <td>Sugar</td>
-            <td>{meal.nutritional_summary?.sugar.toLocaleString("en-US", {maximumFractionDigits: 0})}</td>
+            <td>
+              {meal.nutritional_summary?.sugar.toLocaleString("en-US", {
+                maximumFractionDigits: 0,
+              })}
+            </td>
           </tr>
           <tr>
             <td>Cholesterol</td>
-            <td>{meal.nutritional_summary?.cholesterol.toLocaleString("en-US", {maximumFractionDigits: 0})}</td>
+            <td>
+              {meal.nutritional_summary?.cholesterol.toLocaleString("en-US", {
+                maximumFractionDigits: 0,
+              })}
+            </td>
           </tr>
         </tbody>
       </table>
 
-      <button className="btn btn-warning mt-3" onClick={() => setEditMealVisibility(true)}>
-        Edit Meal
-      </button>
+      {!editMealVisibility ? (
+        <button
+          className="btn btn-warning mt-3"
+          onClick={() => setEditMealVisibility(true)}
+        >
+          Edit Meal
+        </button>
+      ) : (
+        <></>
+      )}
+
       {editMealVisibility ? (
         <>
+          <button
+            className="btn btn-secondary mt-3"
+            onClick={() => setEditMealVisibility(false)}
+          >
+            Cancel Edit
+          </button>
           <h1 className="mt-3">Edit Meal</h1>
           <form onSubmit={handleSubmit}>
-            <div>
-              Name: <input name="name" defaultValue={meal.name} type="text" />
+            <div className="row">
+              <div className="mb-2 col-6">
+                <label htmlFor="mealName" className="form-label">
+                  Name
+                </label>
+                <input
+                  name="name"
+                  type="text"
+                  className="form-control"
+                  id="mealName"
+                  defaultValue={meal.name}
+                />
+              </div>
+              <div className="mb-2 col-6">
+                <label htmlFor="mealPicture" className="form-label">
+                  Picture
+                </label>
+                <input
+                  name="picture"
+                  type="text"
+                  className="form-control"
+                  id="mealPicture"
+                  defaultValue={meal.picture}
+                />
+              </div>
+              <button className="btn btn-warning mt-3 col-6" type="submit">
+                Update Meal
+              </button>
+              <button
+                className="btn btn-danger mt-3 col-6"
+                onClick={handleClick}
+              >
+                Delete Meal
+              </button>
             </div>
-            <div>
-              Picture: <input name="picture" defaultValue={meal.picture} type="text" />
-            </div>
-            <button className="btn btn-warning mt-3" type="submit">
-              Update Meal
-            </button>
           </form>
-          <button className="btn btn-danger mt-3" onClick={handleClick}>
-            Delete Meal
-          </button>
         </>
       ) : (
         <></>
