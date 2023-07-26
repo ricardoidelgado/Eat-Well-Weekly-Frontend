4f3ff10d0a1857164ec5331f4bbb1ca65b2219f8
Ricardo <rickydel19@gmail.com>
2023-07-25 21:17:26 -0500

    Added styling to create ingredient

--- src/IngredientsNew.jsx
+++ src/IngredientsNew.jsx
@@ -8,7 +8,113 @@ export function IngredientsNew(props) {
   return (
     <div>
       <h1>New Ingredient</h1>
-      <form onSubmit={handleSubmit}>
+      <form className="mt-2" onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="mb-2 col-4">
+            <label htmlFor="ingredientName" className="form-label">
+              Name
+            </label>
+            <input
+              name="name"
+              type="text"
+              className="form-control"
+              id="ingredientName"
+            />
+          </div>
+          <div className="mb-2 col-4">
+            <label htmlFor="ingredientPicture" className="form-label">
+              Picture
+            </label>
+            <input
+              name="picture"
+              type="text"
+              className="form-control"
+              id="ingredientPicture"
+            />
+          </div>
+          <div className="mb-2 col-4">
+            <label htmlFor="ingredientCalories" className="form-label">
+              Calories
+            </label>
+            <input
+              name="calories"
+              type="text"
+              className="form-control"
+              id="ingredientCalories"
+            />
+          </div>
+          <div className="mb-2 col-4">
+            <label htmlFor="ingredientFat" className="form-label">
+              Fat
+            </label>
+            <input
+              name="fat"
+              type="text"
+              className="form-control"
+              id="ingredientFat"
+            />
+          </div>
+          <div className="mb-2 col-4">
+            <label htmlFor="ingredientSodium" className="form-label">
+              Sodium
+            </label>
+            <input
+              name="sodium"
+              type="text"
+              className="form-control"
+              id="ingredientSodium"
+            />
+          </div>
+          <div className="mb-2 col-4">
+            <label htmlFor="ingredientCarbs" className="form-label">
+              Carbs
+            </label>
+            <input
+              name="carbs"
+              type="text"
+              className="form-control"
+              id="ingredientCarbs"
+            />
+          </div>
+          <div className="mb-2 col-4">
+            <label htmlFor="ingredientProtein" className="form-label">
+              Protein
+            </label>
+            <input
+              name="protein"
+              type="text"
+              className="form-control"
+              id="ingredientProtein"
+            />
+          </div>
+          <div className="mb-2 col-4">
+            <label htmlFor="ingredientSugar" className="form-label">
+              Sugar
+            </label>
+            <input
+              name="sugar"
+              type="text"
+              className="form-control"
+              id="ingredientSugar"
+            />
+          </div>
+          <div className="mb-2 col-4">
+            <label htmlFor="ingredientCholesterol" className="form-label">
+              Cholesterol
+            </label>
+            <input
+              name="cholesterol"
+              type="text"
+              className="form-control"
+              id="ingredientCholesterol"
+            />
+          </div>
+          <button className="btn btn-success" type="submit">
+            Create Ingredient
+          </button>
+        </div>
+      </form>
+      {/* <form onSubmit={handleSubmit}>
         <div>
           Name: <input name="name" type="text" />
         </div>
@@ -39,7 +145,7 @@ export function IngredientsNew(props) {
         <button className="btn btn-success" type="submit">
           Create Ingredient
         </button>
-      </form>
+      </form> */}
     </div>
   );
 }
