64417b0a3b3a1d649e5e14427efd5747f456ab1d
Ricardo <rickydel19@gmail.com>
2023-08-13 11:41:46 -0500

    Added initial styling to home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,11 @@
 export function Home() {
   return (
     <div id="home">
-      <h1>Welcome to the Meal Planner Home Page</h1>
-      <img src="https://images.unsplash.com/photo-1615937722923-67f6deaf2cc9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZGlubmVyJTIwdGFibGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" />
+      <h1 id="home-header">Welcome to the Meal Planner Home Page</h1>
+      <img
+        id="home-image"
+        src="https://images.unsplash.com/photo-1615937722923-67f6deaf2cc9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZGlubmVyJTIwdGFibGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+      />
     </div>
   );
 }
